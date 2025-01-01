06b1b7e44ae75166f6015a0786b62829fd86dd0a
Silas Abayomi <abayomisilas50@gmail.com>
2025-01-01 23:44:56 +0100

    links

--- app/components/backstore/Sidebar.js
+++ app/components/backstore/Sidebar.js
@@ -76,7 +76,7 @@ export default function Sidebar() {
               </Link>
             </li>
             <li>
-              <Link href="#" className="inline-flex items-center px-4 py-3 text-white bg-blue-600 rounded-lg active w-full " aria-current="page">
+              <Link href="/dashboard/orders" className="inline-flex items-center px-4 py-3 text-white bg-blue-600 rounded-lg active w-full " aria-current="page">
                 <img className='w-4 h-4 me-2' src="/assets/order.svg" alt="" />
                 Orders
               </Link>
@@ -88,14 +88,14 @@ export default function Sidebar() {
               </Link>
             </li>
             <li>
-              <Link href="#" className="inline-flex items-center px-4 py-3 text-white bg-blue-600 rounded-lg active w-full " aria-current="page">
+              <Link href="//dashboard/store" className="inline-flex items-center px-4 py-3 text-white bg-blue-600 rounded-lg active w-full " aria-current="page">
                 <img className='w-4 h-4 me-2' src="/assets/online-store.svg" alt="" />
                 Online Store
               </Link>
             </li>
 
             <li>
-              <Link href="#" className="inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-blue-50 hover:bg-gray-100 w-full text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">
+              <Link href="/dashboard/settings" className="inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-blue-50 hover:bg-gray-100 w-full text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">
                 <svg className="w-4 h-4 me-2 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                   <path d="M18 7.5h-.423l-.452-1.09.3-.3a1.5 1.5 0 0 0 0-2.121L16.01 2.575a1.5 1.5 0 0 0-2.121 0l-.3.3-1.089-.452V2A1.5 1.5 0 0 0 11 .5H9A1.5 1.5 0 0 0 7.5 2v.423l-1.09.452-.3-.3a1.5 1.5 0 0 0-2.121 0L2.576 3.99a1.5 1.5 0 0 0 0 2.121l.3.3L2.423 7.5H2A1.5 1.5 0 0 0 .5 9v2A1.5 1.5 0 0 0 2 12.5h.423l.452 1.09-.3.3a1.5 1.5 0 0 0 0 2.121l1.415 1.413a1.5 1.5 0 0 0 2.121 0l.3-.3 1.09.452V18A1.5 1.5 0 0 0 9 19.5h2a1.5 1.5 0 0 0 1.5-1.5v-.423l1.09-.452.3.3a1.5 1.5 0 0 0 2.121 0l1.415-1.414a1.5 1.5 0 0 0 0-2.121l-.3-.3.452-1.09H18a1.5 1.5 0 0 0 1.5-1.5V9A1.5 1.5 0 0 0 18 7.5Zm-8 6a3.5 3.5 0 1 1 0-7 3.5 3.5 0 0 1 0 7Z" />
                 </svg>
