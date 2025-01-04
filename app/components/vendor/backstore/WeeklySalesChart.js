19c19c3d537f1bcd9b357e113fc0c7e7e0d54dc2
Silas Abayomi <abayomisilas50@gmail.com>
2025-01-04 17:37:00 +0100

    Update WeeklySalesChart.js

--- app/components/vendor/backstore/WeeklySalesChart.js
+++ app/components/vendor/backstore/WeeklySalesChart.js
@@ -88,8 +88,8 @@ export default function WeeklySalesChart() {
 
 
     return (
-        <div className='bg-gray-200 p-8 rounded-lg'>
-            <h2 className='text-blue-600 text-xl font-bold mb-4'>Weekly Sales</h2>
+        <div className='dark:bg-gray-500 bg-gray-200 p-8 rounded-lg'>
+            <h2 className='text-blue-600 dark:text-gray-50 text-xl font-bold mb-4'>Weekly Sales</h2>
 
             <div className='mt-8'>
                 {/*Tabs  */}
@@ -102,7 +102,7 @@ export default function WeeklySalesChart() {
                             tabs.map((tab, i) => {
                                 return (
                                     <li className="me-2" key={i}>
-                                        <button onClick={() => setChartToDisplay(tab.type)} className={chartToDisplay == tab.type ? "inline-flex items-center justify-center p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500 group" : "inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg   group"} >
+                                        <button onClick={() => setChartToDisplay(tab.type)} className={chartToDisplay == tab.type ? "inline-flex items-center justify-center p-4 border-b-2 border-blue-600 rounded-t-lg active text-blue-600 dark:text-gray-50 dark:border-blue-500 group" : "inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg   group"} >
                                             <svg className="w-4 h-4 me-2 text-gray-400  dark:text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                                 <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
                                             </svg>
