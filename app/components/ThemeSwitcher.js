6da9d23eedc0ba860382c8aaedfae2b5b61ba7ad
Silas Abayomi <abayomisilas50@gmail.com>
2025-01-04 16:50:01 +0100

    Update ThemeSwitcher.js

--- app/components/ThemeSwitcher.js
+++ app/components/ThemeSwitcher.js
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react'
 import { useTheme } from 'next-themes'
-import { Sun, } from 'lucide-react'
+import { Moon, Sun, } from 'lucide-react'
 
 export const ThemeChanger = () => {
     const [mounted, setMounted] = useState(false);
@@ -19,7 +19,7 @@ export const ThemeChanger = () => {
 
     return (
         <button className={`w-fit p-2 rounded-md hover:scae-110 active:scale-100 duration-200 bg-slate-200 dark:bg-red-600`} onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
-            {theme === 'light' ? <Sun size={24} /> : <Sun size={24} className="fill-current text-red-600" />}
+            {theme === 'light' ? <Moon/> : <Sun size={24} className="fill-current text-red-600" />}
         </button>
     )
 }
