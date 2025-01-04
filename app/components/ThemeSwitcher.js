cd2f1770537b1aeaf8d5e6db8c9093d343bce9d6
Silas Abayomi <abayomisilas50@gmail.com>
2025-01-04 16:50:26 +0100

    Update ThemeSwitcher.js

--- app/components/ThemeSwitcher.js
+++ app/components/ThemeSwitcher.js
@@ -19,7 +19,7 @@ export const ThemeChanger = () => {
 
     return (
         <button className={`w-fit p-2 rounded-md hover:scae-110 active:scale-100 duration-200 bg-slate-200 dark:bg-red-600`} onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
-            {theme === 'light' ? <Moon/> : <Sun size={24} className="fill-current text-red-600" />}
+            {theme === 'light' ? <Moon/> : <Sun/>}
         </button>
     )
 }
