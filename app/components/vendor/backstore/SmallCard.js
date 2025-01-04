bfc2d2540dae26ff5792e6720ce1bf38ab1e95c8
Silas Abayomi <abayomisilas50@gmail.com>
2025-01-04 17:22:30 +0100

    Update SmallCard.js

--- app/components/vendor/backstore/SmallCard.js
+++ app/components/vendor/backstore/SmallCard.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function SmallCard({ data }) {
     return (
-        <div className={`rounded-lg text-white shadow-md p-8 flex gap-2 items-center bg-gray-200`}>
+        <div className={`rounded-lg text-white shadow-md p-8 flex gap-2 items-center dark:bg-slate-500 bg-gray-200`}>
             <span className={`p-2 rounded-full ${data.iconBg}`}>{data.icon}</span>
-            <div>
-                <p className='text-blue-600'>{data.title}</p>
-                <h2 className='text-xl lg:text-2xl font-bold text-blue-600'>{data.order}</h2>
+            <div className='text-blue-600 dark:text-gray-50'>
+                <p>{data.title}</p>
+                <h2 className='text-xl lg:text-2xl font-bold'>{data.order}</h2>
             </div>
         </div>
     )
