01f13cf0ec50349c9a12d1d3c157b01faaa6e755
Silas Abayomi <abayomisilas50@gmail.com>
2025-01-04 18:31:03 +0100

    Update Sidebar.js

--- app/components/vendor/backstore/Sidebar.js
+++ app/components/vendor/backstore/Sidebar.js
@@ -55,7 +55,7 @@ export default function Sidebar() {
               </Link>
             </li>
             <li>
-              <Link href="/vendor/dashboard/orders" className="inline-flex items-center px-4 py-3 text-white bg-blue-600 rounded-lg active w-full " aria-current="page">
+              <Link href="/vendor/dashboard/orders" className="inline-flex items-center px-4 py-3 text-white bg-blue-700 rounded-lg active w-full " aria-current="page">
                 <img className='w-4 h-4 me-2' src="/assets/order.svg" alt="" />
                 Orders
               </Link>
@@ -67,7 +67,7 @@ export default function Sidebar() {
               </Link>
             </li>
             <li>
-              <Link href="/vendor/dashboard/store" className="inline-flex items-center px-4 py-3 text-white bg-blue-600 rounded-lg active w-full " aria-current="page">
+              <Link href="/vendor/dashboard/store" className="inline-flex items-center px-4 py-3 text-white bg-blue-700 rounded-lg active w-full " aria-current="page">
                 <img className='w-4 h-4 me-2' src="/assets/online-store.svg" alt="" />
                 Online Store
               </Link>
@@ -82,7 +82,7 @@ export default function Sidebar() {
               </Link>
             </li>
             <li>
-              <Link href="#" className="inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-blue-600 hover:bg-gray-100 w-full  dark:hover:bg-gray-700 dark:hover:text-white">
+              <Link href="#" className="inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-blue-700 hover:bg-gray-100 w-full  dark:hover:bg-gray-700 dark:hover:text-white">
                 <svg className="w-4 h-4 me-2 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                   <path d="M7.824 5.937a1 1 0 0 0 .726-.312 2.042 2.042 0 0 1 2.835-.065 1 1 0 0 0 1.388-1.441 3.994 3.994 0 0 0-5.674.13 1 1 0 0 0 .725 1.688Z" />
                   <path d="M17 7A7 7 0 1 0 3 7a3 3 0 0 0-3 3v2a3 3 0 0 0 3 3h1a1 1 0 0 0 1-1V7a5 5 0 1 1 10 0v7.083A2.92 2.92 0 0 1 12.083 17H12a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h1a1.993 1.993 0 0 0 1.722-1h.361a4.92 4.92 0 0 0 4.824-4H17a3 3 0 0 0 3-3v-2a3 3 0 0 0-3-3Z" />
