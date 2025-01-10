0ebf7b560a62f0f8cd1921e38ced651589533d84
Silas Abayomi <abayomisilas50@gmail.com>
2025-01-10 19:42:14 +0100

    Update CustomDataTable.js

--- app/components/vendor/backstore/CustomDataTable.js
+++ app/components/vendor/backstore/CustomDataTable.js
@@ -75,7 +75,7 @@ export default function CustomDataTable() {
                                             {item.category}
                                         </td>
                                         <td className="px-6 py-4">
-                                            {item.gender}
+                                            {item.price}
                                         </td>
                                         <td className="px-6 py-4">
                                             <a href="#" className="font-medium text-blue-600 dark:text-yellow-500 hover:underline">Edit</a>
